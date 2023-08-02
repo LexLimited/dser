8822701f17f5fefe084028e0fe24330c65019057
AlexanderPVl <shggv@yandex.ru>
2023-08-02 12:46:11 -0500

    git: remove unnecessary files from git

--- run.sh
+++
@@ -1,8 +0,0 @@
-rm -rf build/*
-cmake -B build
-cd build
-make
-echo "
-----------------------------------------
-"
-time ./dser
--- update_clang_config.sh
+++
@@ -1,2 +0,0 @@
-cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=true
-rm -r CMakeFiles CMakeCache.txt Makefile cmake_install.cmake
